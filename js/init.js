dacf2fa6c5edec709b2bea9fc5f53ca01c1b40b6
impmihai <mihai.lupea@gmail.com>
2017-03-01 15:40:36 +0200

    prima versiune uploadata

--- js/init.js
+++ js/init.js
@@ -23,6 +23,7 @@
     });
     $('window.hash')
     $('#nav a').on('click tap', function(e){
+        e.preventDefault();
         var target = this.hash;
     	var $target = $(target);
         if($target.length)
